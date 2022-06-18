8fd66c7f0198ed1c776f46c806f10645c2a58ff5
dgoto2 <daisuke.goto@hi.no>
2022-06-18 10:27:33 +0200

    add plot functions

--- analyse_mse.R
+++ analyse_mse.R
@@ -159,7 +159,7 @@ combs <- data.frame(name = c("F0","A*", "base", "10", "20", "30", "40", "50"),
   biasN = c(0.0, 0.0, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5),
   biasF = c(0.0, 0.0, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5),
   scenario = 0) 					
-combs<-combs[c(3:8),] # select only the scenarios read in from the output files
+combs<-combs[c(3:8),] # select the scenarios read in from the output files
 
 combs1 <- stats %>% left_join(combs)
 combs1$name <- c("base","10", "20", "30", "40", "50")
@@ -187,30 +187,12 @@ stats_full <- function(data) {
                                        key = "catch_long",
                                        value = c(window(catch(stk_i@stock), start = 2029))),
                             data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
-                                       key = "catch_medium",
-                                       value = c(window(catch(stk_i@stock), start = 2024, end = 2028))),
-                            data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
                                        key = "catch_short",
                                        value = c(window(catch(stk_i@stock), start = 2019, end = 2023))),
-                            data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
-                                       key = "risk1_long",
-                                       value = mean(window(ssb(stk_i@stock), start = 2029) < Blim)),
-                            data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
-                                       key = "risk1_medium",
-                                       value = mean(window(ssb(stk_i@stock), 
-                                                           start = 2024, end = 2028) < Blim)),
-                            data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
-                                       key = "risk1_short",
-                                       value = mean(window(ssb(stk_i@stock), 
-                                                           start = 2019, end = 2023) < Blim)),
                             data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
                                        key = "risk3_long",
                                        value = max(iterMeans(window(ssb(stk_i@stock), 
                                                                     start = 2029) < Blim))),
-                            data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
-                                       key = "risk3_medium",
-                                       value = max(iterMeans(window(ssb(stk_i@stock), 
-                                                                    start = 2024, end = 2028) < Blim))),
                             data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
                                        key = "risk3_short",
                                        value = max(iterMeans(window(ssb(stk_i@stock), 
@@ -219,20 +201,12 @@ stats_full <- function(data) {
                                        key = "iav_long",
                                        value = c(iav(object = catch(window(stock(stk_i), start = 2028))))),
                             data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
-                                       key = "iav_medium",
-                                       value = c(iav(object = catch(window(stock(stk_i), 
-                                                                           start = 2023, end = 2028))))),
-                            data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
                                        key = "iav_short",
                                        value = c(iav(object = catch(window(stock(stk_i), 
                                                                            start = 2018, end = 2023))))),
                             data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
                                        key = "iavssb_long",
                                        value = c(iav(object = ssb(window(stock(stk_i), start = 2028))))),
-                            data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
-                                       key = "iavssb_medium",
-                                       value = c(iav(object = ssb(window(stock(stk_i), 
-                                                                         start = 2023, end = 2028))))),
                             data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
                                        key = "iavssb_short",
                                        value = c(iav(object = ssb(window(stock(stk_i), 
@@ -241,29 +215,18 @@ stats_full <- function(data) {
                                        key = "iavfbar_long",
                                        value = c(iav(object = fbar(window(stock(stk_i), start = 2028))))),
                             data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
-                                       key = "iavfbar_medium",
-                                       value = c(iav(object = fbar(window(stock(stk_i), 
-                                                                          start = 2023, end = 2028))))),
-                            data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
                                        key = "iavfbar_short",
                                        value = c(iav(object = fbar(window(stock(stk_i), 
                                                                           start = 2018, end = 2023))))),
-                            
                             data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
                                        key = "ssb_long",
                                        value = c(window(ssb(stk_i@stock), start = 2029))),
-                            data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
-                                       key = "ssb_medium",
-                                       value = c(window(ssb(stk_i@stock), start = 2024, end = 2028))),
                             data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
                                        key = "ssb_short",
                                        value = c(window(ssb(stk_i@stock), start = 2019, end = 2023))),
                             data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
                                        key = "F_median_long",
                                        value = c(window(fbar(stk_i@stock), start = 2029))),
-                            data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
-                                       key = "F_median_medium",
-                                       value = c(window(fbar(stk_i@stock), start = 2024, end = 2028))),
                             data.frame(name = i$name, scenario = i$scenario, biasN = i$biasN, biasF = i$biasF,
                                        key = "F_median_short",
                                        value = c(window(fbar(stk_i@stock), start = 2019, end = 2023))),
@@ -279,8 +242,8 @@ stats_full <- function(data) {
                                                          if (any(x)) {which(x)[1]} else {Inf}})))
                           )
                           if (i$HCR == "F") {
-                            res$value[res$key %in% c("catch_long", "catch_medium", "catch_short",
-                                                     "iav_long", "iav_medium", "iav_short")] <- 0
+                            res$value[res$key %in% c("catch_long",  "catch_short",
+                                                     "iav_long", "iav_short")] <- 0
                           }
                           res <- merge(res, i[, c("name", "OM", "HCR", "BB", "TACconstr", "Btrigger",
                                                   "Ftrgt", "biasN", "biasF")])
@@ -302,98 +265,6 @@ ggplot(data = combs_base,
   theme_bw() +
   ylim(0, NA)
 levels(combs_base$name) <- c("F0","A*","base", "10%", "20%", "30%", "40%", "50%")
-
-### get median for option A* or base
-combs_base_b <- left_join(combs_base_b, 
-                          combs_base_b %>%
-                            group_by(key, OM, name) %>%
-                            summarise(value_median = median(value)) %>%
-                            filter(name == "base") %>%
-                            select(-name))
-### perceived biased
-p_iavssb_long_b <- ggplot(data = combs_base_b[combs_base_b$key == "iavssb_long", ], 
-                          mapping = aes(x = name, y = value)) +
-  geom_violin(trim=FALSE, aes(fill = name), lwd = 0.2, show.legend = FALSE) + 
-  geom_boxplot(width=0.25, outlier.size = 0.8, outlier.alpha = 0.5, lwd = 0.2, fill = "white") + 
-  theme_classic() + ylim(0, NA) + scale_fill_jco() + 
-  geom_hline(aes(yintercept = value_median), colour = "red", alpha = 0.5) +
-  coord_cartesian(ylim = c(0, 1)) + 
-  labs(x = "", y = "") +
-  labs(x = "", y = "Interannual variability \nin SSB") +
-  theme(axis.text.x = element_blank()) 
-p_iavssb_short_b <- ggplot(data = combs_base_b[combs_base_b$key == "iavssb_short", ], 
-                           mapping = aes(x = name, y = value)) +
-  geom_violin(trim=FALSE, aes(fill = name), lwd = 0.2, show.legend = FALSE) + 
-  geom_boxplot(width=0.2, outlier.size = 0.8, outlier.alpha = 0.5, lwd = 0.2, fill = "white") + 
-  theme_classic() + ylim(0, NA) + scale_fill_jco() +
-  geom_hline(aes(yintercept = value_median), colour = "red", alpha = 0.5) +
-  coord_cartesian(ylim = c(0, 1)) + 
-  labs(x = "", y = "") +
-  labs(x = "", y = "Interannual variability \nin SSB") +
-  theme(axis.text.x = element_blank()) 
-p_iavfbar_long_b <- ggplot(data = combs_base_b[combs_base_b$key == "iavfbar_long", ], 
-                           mapping = aes(x = name, y = value)) +
-  geom_violin(trim=FALSE, aes(fill = name), lwd = 0.2, show.legend = FALSE) + 
-  geom_boxplot(width=0.25, outlier.size = 0.8, outlier.alpha = 0.5, lwd = 0.2, fill = "white") + 
-  theme_classic() + ylim(0, NA) + scale_fill_jco() + 
-  geom_hline(aes(yintercept = value_median), colour = "red", alpha = 0.5) +
-  coord_cartesian(ylim = c(0, 2)) + 
-  labs(x = "", y = "") +
-  labs(x = "", y = "Variability in \nfishing mortality rate") +
-  theme(axis.text.x = element_blank()) 
-p_iavfbar_short_b <- ggplot(data = combs_base_b[combs_base_b$key == "iavfbar_short", ], 
-                            mapping = aes(x = name, y = value)) +
-  geom_violin(trim=FALSE, aes(fill = name), lwd = 0.2, show.legend = FALSE) + 
-  geom_boxplot(width=0.2, outlier.size = 0.8, outlier.alpha = 0.5, lwd = 0.2, fill = "white") + 
-  theme_classic() + ylim(0, NA) + scale_fill_jco() +
-  geom_hline(aes(yintercept = value_median), colour = "red", alpha = 0.5) +
-  coord_cartesian(ylim = c(0, 2)) + 
-  labs(x = "", y = "") +
-  labs(x = "", y = "Variability in \nfishing mortality rate") +
-  theme(axis.text.x = element_blank())
-p_ssb_long_b <- ggplot(data = combs_base_b[combs_base_b$key == "ssb_long", ], 
-                       mapping = aes(x = name, y = value/1000)) +
-  geom_violin(trim=FALSE, aes(fill = name), lwd = 0.2, show.legend = FALSE) + 
-  geom_boxplot(width=0.25, outlier.size = 0.8, outlier.alpha = 0.5, lwd = 0.2, fill = "white") + 
-  theme_classic() + ylim(0, NA) + scale_fill_jco() + 
-  geom_hline(aes(yintercept = value_median/1000), colour = "red", alpha = 0.5) +
-  theme(axis.text.x = element_blank()) +  
-  coord_cartesian(ylim = c(0, 1200)) +
-  labs(x = "", y = "") +
-  labs(x = "", y = "SSB [1000 t]") +
-  scale_y_continuous(labels = function(x) format(x, scientific = F, digits = 2))
-p_ssb_short_b <- ggplot(data = combs_base_b[combs_base_b$key == "ssb_short", ], 
-                        mapping = aes(x = name, y = value/1000)) +
-  geom_violin(trim=FALSE, aes(fill = name), lwd = 0.2, show.legend = FALSE) + 
-  geom_boxplot(width=0.25, outlier.size = 0.8, outlier.alpha = 0.5, lwd = 0.2, fill = "white") + 
-  theme_classic() + ylim(0, NA) + scale_fill_jco() +
-  geom_hline(aes(yintercept = value_median/1000), colour = "red", alpha = 0.5) +
-  theme(axis.text.x = element_blank()) +  
-  coord_cartesian(ylim = c(0, 1000)) +
-  labs(x = "", y = "") +
-  labs(x = "", y = "SSB [1000 t]") +
-  scale_y_continuous(labels = function(x) format(x, scientific = F, digits = 2),
-                     limits = c(0, NA))
-p_fbar_long_b <- ggplot(data = combs_base_b[combs_base_b$key == "F_median_long", ], 
-                        mapping = aes(x = name, y = value)) +
-  geom_violin(trim=FALSE, aes(fill = name), lwd = 0.2, show.legend = FALSE) + 
-  geom_boxplot(width=0.25, outlier.size = 0.8, outlier.alpha = 0.5, lwd = 0.2, fill = "white") + 
-  theme_classic() + ylim(0, NA) + scale_fill_jco() + 
-  geom_hline(aes(yintercept = value_median), colour = "red", alpha = 0.5) +
-  ylim(0, 2) +
-  labs(x = "", y = "Fishing mortality rate") +
-  theme(legend.direction = "horizontal") +
-  theme(axis.text.x = element_text(angle = 90, vjust=0.2)) 
-p_fbar_short_b <- ggplot(data = combs_base_b[combs_base_b$key == "F_median_short", ], 
-                         mapping = aes(x = name, y = value)) +
-  geom_violin(trim=FALSE, aes(fill = name), lwd = 0.2, show.legend = FALSE) + 
-  geom_boxplot(width=0.25, outlier.size = 0.8, outlier.alpha = 0.5, lwd = 0.2, fill = "white") + 
-  theme_classic() + ylim(0, NA) + scale_fill_jco() + 
-  geom_hline(aes(yintercept = value_median), colour = "red", alpha = 0.5) +
-  ylim(0, 2) +
-  labs(x = "", y = "Fishing mortality rate") +
-  theme(legend.direction = "horizontal") +
-  theme(axis.text.x = element_text(angle = 90, vjust=0.2)) 
 
 ### get median for option A* or base
 combs_base <- left_join(combs_base, 
@@ -519,16 +390,6 @@ p_catch_long <- ggplot(data = combs_base[combs_base$key == "catch_long", ],
   labs(x = "", y = "Catch [1000 t]") +
   theme(axis.text.x = element_blank()) +
   scale_y_continuous(labels = function(x) format(x, scientific = F, digits = 1))
-p_catch_medium <- ggplot(data = combs_base[combs_base$key == "catch_medium", ], 
-                         mapping = aes(x = name, y = value/1000)) +
-  geom_violin(trim=FALSE, aes(fill = name), lwd = 0.2, show.legend = FALSE) + 
-  geom_boxplot(width=0.25, outlier.size = 0.8, outlier.alpha = 0.5, lwd = 0.2, fill = "white") + 
-  theme_classic() + ylim(0, NA) + scale_fill_jco() +
-  geom_hline(aes(yintercept = value_median/1000), colour = "red", alpha = 0.5) +
-  theme(axis.text.x = element_blank()) +  
-  labs(x = "", y = "Catch [t]") +
-  labs(x = "", y = "") +
-  scale_y_continuous(labels = function(x) format(x, scientific = F, digits = 1))
 p_catch_short <- ggplot(data = combs_base[combs_base$key == "catch_short", ], 
                         mapping = aes(x = name, y = value/1000)) +
   geom_violin(trim=FALSE, aes(fill = name), lwd = 0.2, show.legend = FALSE) + 
@@ -549,15 +410,6 @@ p_risk3_long <- ggplot(data = combs_base[combs_base$key == "risk3_long", ],
   labs(x = "", y = "") +
   labs(x = "", y = "Risk") +
   theme(axis.text.x = element_text(angle = 90, vjust=0.2)) 
-p_risk3_medium <- ggplot(data = combs_base[combs_base$key == "risk3_medium", ], 
-                         mapping = aes(x = name, y = value)) +
-  geom_bar(stat = "identity", position = "dodge", lwd = 0.2, color = "black", aes(fill = name), show.legend = FALSE) + scale_fill_jco() +
-  geom_blank(data = combs_base[combs_base$key == "risk1_medium", ]) +
-  geom_hline(aes(yintercept = 0.05), colour = "red", alpha = 0.5) +
-  theme_classic() + ylim(0, 0.45) +
-  labs(x = "", y = "Risk") +
-  labs(x = "", y = "") +
-  theme(axis.text.x = element_text(angle = 90, vjust=0.2)) 
 p_risk3_short <- ggplot(data = combs_base[combs_base$key == "risk3_short", ], 
                         mapping = aes(x = name, y = value)) +
   geom_bar(stat = "identity", position = "dodge", lwd = 0.2, color = "black", aes(fill = name), show.legend = FALSE) + scale_fill_jco() +
@@ -577,15 +429,6 @@ p_iav_long <- ggplot(data = combs_base[combs_base$key == "iav_long", ],
   labs(x = "", y = "") +
   labs(x = "", y = "Interannual catch\n variability") +
   theme(axis.text.x = element_blank()) 
-p_iav_medium <- ggplot(data = combs_base[combs_base$key == "iav_medium", ], 
-                       mapping = aes(x = name, y = value)) +
-  geom_violin(trim=FALSE, aes(fill = name), lwd = 0.2, show.legend = FALSE) + 
-  geom_boxplot(width=0.25, outlier.size = 0.8, outlier.alpha = 0.5, lwd = 0.2, fill = "white") + 
-  theme_classic() + ylim(0, NA) + scale_fill_jco() +
-  geom_hline(aes(yintercept = value_median), colour = "red", alpha = 0.5) +
-  theme(axis.text.x = element_blank()) +  
-  coord_cartesian(ylim = c(0, 2)) + 
-  labs(x = "", y = "") 
 p_iav_short <- ggplot(data = combs_base[combs_base$key == "iav_short", ], 
                       mapping = aes(x = name, y = value)) +
   geom_violin(trim=FALSE, aes(fill = name), lwd = 0.2, show.legend = FALSE) + 
@@ -624,4 +467,3 @@ plot_grid(p_catch_long, p_risk1_long, p_iavssb_long,
           ncol=1,align = "hv")
 plot_grid(p_recovery_proportion, p_recovery_time,ncol=1,
           align = "hv")
-
